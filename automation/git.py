188bf1398724901c2fc910b6f0650c8bbc6e915c
Sidharth Menon Poonath <spoonath@shoretel.com>
2018-06-26 13:22:39 +0530

    new commit

--- automation/git.py
+++ automation/git.py
@@ -2,13 +2,15 @@ import git
 # from git import Repo
 puppet_module = 'hieradata'
 absolute_path_to_file = 'automation/test2.txt'
+commit_message =
 
 path_to_puppet_module = 'git/puppet/' + puppet_module
 repo = git.Repo(path_to_puppet_module)
+gitobj = repo.git
 
 
 def git_master_branch():
-    repo.git.checkout('master')
-    repo.git.add(absolute_path_to_file)
-    repo.git.commit('-am', message)
-    repo.git.push()
+    gitobj.checkout('master')
+    gitobj.add(absolute_path_to_file)
+    gitobj.commit('-am', commit_message)
+    gitobj.push()
