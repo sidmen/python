5490c888340a61b504f5236ac915234065649839
Sidharth Menon Poonath <spoonath@shoretel.com>
2018-06-26 13:10:05 +0530

    git.py file

--- automation/git.py
+++ automation/git.py
@@ -1,4 +1,14 @@
 import git
 # from git import Repo
-repo = git.Repo('git/puppet/hieradata')
-print(repo.git.status())
+puppet_module = 'hieradata'
+absolute_path_to_file = 'automation/test2.txt'
+
+path_to_puppet_module = 'git/puppet/' + puppet_module
+repo = git.Repo(path_to_puppet_module)
+
+
+def git_master_branch():
+    repo.git.checkout('master')
+    repo.git.add(absolute_path_to_file)
+    repo.git.commit('-am', message)
+    repo.git.push()
